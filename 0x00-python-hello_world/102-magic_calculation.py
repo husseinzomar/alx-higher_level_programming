7ed77078c9db1fc0ed7bfe8d03d581f20a65f0f0
Hussein AL-zomar <68166543+husseinzomar@users.noreply.github.com>
2024-01-02 13:27:54 +0200

    Delete 0x00-python-hello_world directory

--- 0x00-python-hello_world/102-magic_calculation.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""calculation"""
-
-
-def magic_calculation(a, b):
-    """does exactly same as bytecode given"""
-    const = 98
-    result = pow(a, b) + const
-    return result
