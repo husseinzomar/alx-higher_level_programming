efc860235fff7d57ac726ff3723735bcf8356036
Hussein AL-zomar <68166543+husseinzomar@users.noreply.github.com>
2024-01-10 16:28:49 +0200

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import hidden_4
-    d = dir
-    for i in range(0, len(d)):
-        if d[i][0:2] != "__":
-            print("{}".format(d[i]))
+    names = dir(hidden_4)
+    for arg in names:
+        if '__' in arg:
+            continue
+        print('{}'.format(arg))
