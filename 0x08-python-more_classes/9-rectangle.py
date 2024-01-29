dfd8fd0a24664bf5916d084d4ba56339472015ed
Hussein AL-zomar <68166543+husseinzomar@users.noreply.github.com>
2024-01-29 16:31:33 +0200

    Update 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -18,13 +18,12 @@ class Rectangle:
 
    @width.setter
     def width(self, value):
-        """To set attribute"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
-
-        self.__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
